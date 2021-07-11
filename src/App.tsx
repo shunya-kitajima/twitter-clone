93e05092f448d3c9c2a8f369fce2e1ca14801da6
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-07-12 01:00:53 +0900

    useEffectを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-function App() {
+import styles from './App.module.css';
+import Auth from './components/Auth';
+import Feed from './components/Feed';
+import { selectUser, login, logout } from './features/userSlice';
+import { auth } from './firebase';
+
+
+const App: React.FC = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    };
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      {user.uid ? (
+        <div className={styles.app}>
+          <Feed/>
+        </div>
+      ) : (
+        <Auth/>
+      )}
+    </>
   );
 }
 
