6d2409dbb5b04744017a364fdda5f79c5b41fe67
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-07-19 22:46:48 +0900

    投稿情報の表示欄にスタイルを追加

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -10,8 +10,43 @@ import styles from "./Post.module.css";
 import { db } from "../firebase";
 import { selectUser } from "../features/userSlice";
 
-const Post = () => {
-  return <div></div>;
+interface PROPS {
+  postId: string;
+  avatar: string;
+  image: string;
+  text: string;
+  timestamp: any;
+  username: string;
+}
+
+const Post: React.FC<PROPS> = (props) => {
+  return (
+    <div className={styles.post}>
+      <div className={styles.post_avatar}>
+        <Avatar src={props.avatar} />
+      </div>
+      <div className={styles.post_body}>
+        <div>
+          <div className={styles.post_header}>
+            <h3>
+              <span className={styles.post_headerUser}>@{props.username}</span>
+              <span className={styles.post_headerTime}>
+                {new Date(props.timestamp?.toDate()).toLocaleString()}
+              </span>
+            </h3>
+          </div>
+          <div className={styles.post_tweet}>
+            <p>{props.text}</p>
+          </div>
+        </div>
+        {props.image && (
+          <div className={styles.post_tweetImage}>
+            <img src={props.image} alt="tweet" />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Post;
