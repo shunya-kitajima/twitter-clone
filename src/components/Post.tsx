3e0d4ef6d37bcc7400f4c4b386ec7813c87896a7
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-07-20 00:58:01 +0900

    コメント送信機能を追加

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -20,6 +20,20 @@ interface PROPS {
 }
 
 const Post: React.FC<PROPS> = (props) => {
+  const [comment, setComment] = useState("");
+  const user = useSelector(selectUser);
+
+  const newComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    db.collection("posts").doc(props.postId).collection("comments").add({
+      avatar: user.photoUrl,
+      text: comment,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      username: user.displayName,
+    });
+    setComment("");
+  };
+
   return (
     <div className={styles.post}>
       <div className={styles.post_avatar}>
@@ -44,6 +58,28 @@ const Post: React.FC<PROPS> = (props) => {
             <img src={props.image} alt="tweet" />
           </div>
         )}
+        <form onSubmit={newComment}>
+          <div className={styles.post_form}>
+            <input
+              className={styles.post_input}
+              type="text"
+              placeholder="Type new comment..."
+              value={comment}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setComment(e.target.value)
+              }
+            />
+            <button
+              disabled={!comment}
+              className={
+                comment ? styles.post_button : styles.post_buttonDisable
+              }
+              type="submit"
+            >
+              <SendIcon className={styles.post_sendIcon} />
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
